fb3f6be985d87deff92cbde409857c85fcbee687
BeepoBeeping <561749@escg.ac.uk>
2025-12-10 22:33:37 +0000

    i did a menu
    it is good

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,79 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public float musicVolume, sfxVolume;
+    public static AudioManager instance;
+
+    public Sound[] sounds;
+    AudioSource audioSource; //reference to the audio source component on the game object
+
+    void Awake()
+    {
+        // if instance is null, store a reference to this instance
+        if (instance == null)
+        {
+            // a reference does not exist, so store it
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // Another instance of this gameobject has been made so destroy it
+            // as we already have one
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach( Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+
+        }
+    }
+
+    private void Start()
+    {
+        ChangeMusicVolume(PlayerPrefs.GetFloat("musicVolume"));
+        PlayClip("MenuBG");
+    }
+
+    public void PlayClip(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+
+    public void StopClip()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Stop(); //stop currently playing clip
+    }
+
+    public void ChangeAudioSourceVolume(string name, float vol)
+    {
+        Sound s = Array.Find(sounds, AudioSystem => AudioSystem.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "Not found!");
+            return;
+        }
+        s.source.volume = vol;
+
+
+    }
+
+
+    public void ChangeMusicVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+
+
+}
---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,40 @@
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    Sprite sprite;
+    Sprite highlightSprite;
+
+    public TMP_Text buttonText;
+
+    public GameObject player;
+
+    public void L1Button()
+    {
+        SceneManager.LoadScene("Level1");
+        FindFirstObjectByType<AudioManager>().PlayClip("ButtonClick");
+    }
+    public void ButtonClick()
+    {
+        FindFirstObjectByType<AudioManager>().PlayClip("ButtonClick");
+    }
+
+    public void ButtonBack()
+    {
+        FindFirstObjectByType<AudioManager>().PlayClip("ButtonBack");
+    }
+
+    public void ButtonHover()
+    {
+        FindFirstObjectByType<AudioManager>().PlayClip("ButtonHover");
+    }
+
+    public void OnMouseEnter()
+    {
+        ButtonHover();
+    }
+
+}
